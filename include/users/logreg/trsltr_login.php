03534258a4d61bb2e6725f5d1803fbcd3a292a38
bige1997372 <61354621+bige1997372@users.noreply.github.com>
2020-11-26 20:50:26 +0800

    Add files via upload

--- include/users/logreg/trsltr_login.php
+++ include/users/logreg/trsltr_login.php
@@ -1,7 +1,7 @@
-// 段孝辰
-<?php
 
-include('..\include\conn.php');
+<?php
+// 段孝辰
+include('..\..\conn.php');
 
 session_start();
 //注销登录
@@ -9,15 +9,13 @@ error_reporting(0);
 if($_GET['action'] == "logout"){
     unset($_SESSION['userid']);
     unset($_SESSION['username']);
-    echo 'Log out successfully!点击此处 <a href="login.php">登录</a>';
+    echo "<script>alert('注销成功!');location.href='../../../index.html';</script>";
     exit;
 }
 
 //登录
-if(!isset($_POST['submit'])){
-    exit('非法访问!');
-}
-$username = htmlspecialchars($_POST['trsltrname']);
+
+$username = htmlspecialchars($_POST['username']);
 $password = MD5($_POST['password']);
 
 
@@ -28,7 +26,7 @@ if($result = mysqli_fetch_array($check_query, MYSQLI_ASSOC)){
     //登录成功
     $_SESSION['username'] = $username;
     $_SESSION['userid'] = $result['uid'];
-    echo '<script type="text/javascript">alert("登陆成功");window.location.href="login.php";</script>';
+    echo '<script type="text/javascript">alert("登陆成功");window.location.href="../../../index.html";</script>';
 	
     exit;
 } else {
