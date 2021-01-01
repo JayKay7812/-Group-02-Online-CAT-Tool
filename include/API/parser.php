1e9992e6a322238ac30c3d02e181e599d1f9f2c1
JayKay7812 <jk_qinhaoyang@outlook.com>
2021-01-01 21:45:20 +0800

    更新了parser.php
    现在可以对中文文件进行分句了。
    还有两个问题：1.无法保留句尾标点符号；2.中文分完句会有一个空白记录被添加到数据库。

--- include/API/parser.php
+++ include/API/parser.php
@@ -5,6 +5,8 @@
   use \Smalot\PdfParser\Parser;
   use PhpOffice\PhpWord\PhpSpreadsheet;
   $file = $destination;
+  echo($sourceLanguage);
+  //文件解析
   if ($ftype=="docx")
   {
     require 'PhpWord/vendor/autoload.php';
@@ -25,6 +27,7 @@
         }
       }
     }
+    print($srctxt);
   }
   else if($ftype=="pdf")
   {
@@ -56,44 +59,65 @@
           }
         }
     }
-
-  $special[0] = array();
-  $special[1] = array();
-  //替换特殊的
-  $srctxt = special_replace("/www\.[\w]+\.(com|cn|org)/i",$srctxt);
-  $srctxt = special_replace("/\.(com|cn|org)/i",$srctxt);
-  $srctxt = special_replace("/[0-9]\.[0-9]/",$srctxt);
-  //分句
-  $temp =preg_split("/[\?\.\!]\s?/",trim($srctxt));
-  array_pop($temp);
-  //还原每句
-  foreach($temp as $k => $v)
+  //英文分句
+  if ($sourceLanguage=="en-US")
   {
-    $temp[$k] = special_revert($v);
-    $sql="INSERT INTO translationbase (translationsheet_ID, sourceText, translation_Property) VALUES (0, '$temp[$k]', 12)";
-    if (mysqli_query($conn, $sql)) {
-      echo "新记录插入成功";
-    } else {
-      echo '<script type="text/javaScript">alert("导入文件失败！");点击此处 <a href="javascript:history.back(-1);">返回</a> 重试</script>';
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    $special[0] = array();
+    $special[1] = array();
+    //替换特殊的
+    $srctxt = special_replace("/www\.[\w]+\.(com|cn|org)/i",$srctxt);
+    $srctxt = special_replace("/\.(com|cn|org)/i",$srctxt);
+    $srctxt = special_replace("/[0-9]\.[0-9]/",$srctxt);
+    //分句
+    $temp =preg_split("/[\?\.\!]\s?/",trim($srctxt));
+    array_pop($temp);
+    //还原每句
+    foreach($temp as $k => $v)
+    {
+      $temp[$k] = special_revert($v);
+      $sql="INSERT INTO translationbase (translationsheet_ID, sourceText, translation_Property) VALUES (0, '$temp[$k]', 12)";
+      if (mysqli_query($conn, $sql)) {
+        echo "新记录插入成功";
+      } else {
+        echo '<script type="text/javaScript">alert("导入文件失败！");点击此处 <a href="javascript:history.back(-1);">返回</a> 重试</script>';
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+      }
+    }
+
+    function special_replace($pattern, $str){
+        global $special;
+        preg_match_all($pattern, $str, $temp);
+        if(is_array($temp))
+            foreach($temp[0] as $k => $v){
+                $special[0][] = $v;
+                $special[1][] = $temp2 = "|".md5($v)."|";
+                $str = str_replace($v, $temp2, $str);
+            }
+        return $str;
+    }
+
+    function special_revert($str){
+        global $special;
+        return str_replace($special[1],$special[0],$str);
     }
   }
-
-  function special_replace($pattern, $str){
-      global $special;
-      preg_match_all($pattern, $str, $temp);
-      if(is_array($temp))
-          foreach($temp[0] as $k => $v){
-              $special[0][] = $v;
-              $special[1][] = $temp2 = "|".md5($v)."|";
-              $str = str_replace($v, $temp2, $str);
-          }
-      return $str;
+  //中文分句
+  else if($sourceLanguage=="zh-CN")
+  {
+    print("zh_CN");
+    //分句
+    $temp = preg_split("/(\n|，|。|！|？|；)/", $srctxt);
+    //还原每句
+    foreach($temp as $k => $v)
+    {
+      //$temp[$k] = special_revert($v);
+      $sql="INSERT INTO translationbase (translationsheet_ID, sourceText, translation_Property) VALUES (0, '$temp[$k]', 12)";
+      if (mysqli_query($conn, $sql)) {
+        echo "新记录插入成功";
+      } else {
+        echo '<script type="text/javaScript">alert("导入文件失败！");点击此处 <a href="javascript:history.back(-1);">返回</a> 重试</script>';
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+      }
+    }
   }
-
-  function special_revert($str){
-      global $special;
-      return str_replace($special[1],$special[0],$str);
-  }
-
 ?>
